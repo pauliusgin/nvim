a0feda07f60dee5a04a07ee778e5a2dd09567e5c
Paulius <paulius000@gmail.com>
2024-01-05 23:54:28 +0200

    feat: another attempt  at this

---
+++ lua/keymaps.lua
@@ -0,0 +1,8 @@
+-- leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- file explorer
+vim.keymap.set("n", "<leader>e", ":Lex 30<cr>", { noremap = true, silent = true } )
+
+
---
+++ lua/settings.lua
@@ -0,0 +1,8 @@
+-- file exlorer style
+vim.g.netrw_liststyle = 3
+
+-- hide default comments in file explorer
+vim.g.netrw_banner = 0
+
+-- browsing directory is current directory
+vim.g.netrw_keepdir = 0
