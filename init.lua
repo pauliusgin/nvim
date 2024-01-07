932f18c692b405d7b6b470b9c60c9514181496af
Paulius <paulius000@gmail.com>
2024-01-07 08:14:20 +0200

    feat: completion configured 99%

--- init.lua
+++ init.lua
@@ -6,23 +6,21 @@ require("settings")
 ------------- packet manager Lazy.nvim ---------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 --------------------------------------------------------------
 
--- require'lspconfig'.lua_ls.setup{}
-
 -- colorscheme
 vim.cmd("colorscheme codedark")
 vim.api.nvim_set_hl(0, "Normal", { bg = none })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = none })
+vim.api.nvim_set_hl(0, "NormalFlat", { bg = none })
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,42 +1,134 @@
 return {
-    {'williamboman/mason.nvim',
-    config = function()
-        local lsp_zero = require('lsp-zero')
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        config = function()
+            local lsp_zero = require('lsp-zero')
 
-        lsp_zero.on_attach(function(client, bufnr)
-            lsp_zero.default_keymaps({buffer = bufnr})
-        end)
+            lsp_zero.on_attach(function(_, bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr })
+                local opts = { buffer = bufnr }
+                vim.keymap.set("n", "<leader>f", ":lua vim.lsp.buf.format({ async = true })<cr>", opts)
+                vim.keymap.set("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<cr>", opts)
+            end)
 
-        lsp_zero.extend_lspconfig()
+            lsp_zero.extend_lspconfig()
 
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-            ensure_installed = {
-                "lua_ls",
-                "tsserver",
-                "html",
-                "cssls",
-                "emmet_language_server",
-                "emmet_ls",
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "tsserver",
+                    "html",
+                    "cssls",
+                    "emmet_language_server",
+                    "emmet_ls",
+                },
+                handlers = {
+                    lsp_zero.default_setup,
+                },
+            })
 
-            },
-            handlers = {
-                lsp_zero.default_setup,
-            },
-        })
-    end
-},
+            local cmp = require('cmp')
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-{'williamboman/mason-lspconfig.nvim'},
+            cmp.setup({
+                sources = {
+                    { name = 'path' },
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip', keyword_length = 2 },
+                    { name = 'buffer',  keyword_length = 3 },
+                },
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                matching = {
+                    disallow_fuzzy_matching = true,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                view = {
+                    docs = {
+                        auto_open = false,
+                    },
+                },
+                formatting = lsp_zero.cmp_format(),
+                mapping = cmp.mapping.preset.insert({
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                    ['<C-d>'] = function()
+                        if cmp.visible_docs() then
+                            cmp.close_docs()
+                        else
+                            cmp.open_docs()
+                        end
+                    end
+                }),
+            })
 
-{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+            cmp.setup.cmdline({ "/", "?" }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
 
-{'neovim/nvim-lspconfig'},
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    { name = "cmdline" },
+                }),
+            })
 
-{'hrsh7th/cmp-nvim-lsp'},
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            require('lspconfig')['lua_ls'].setup {
+                capabilities = capabilities
+            }
+            require('lspconfig')['tsserver'].setup {
+                capabilities = capabilities
+            }
+            require('lspconfig')['html'].setup {
+                capabilities = capabilities
+            }
+            require('lspconfig')['cssls'].setup {
+                capabilities = capabilities
+            }
+            require('lspconfig')['emmet_language_server'].setup {
+                capabilities = capabilities
+            }
+            require('lspconfig')['emmet_ls'].setup {
+                capabilities = capabilities
+            }
+        end
+    },
 
-{'hrsh7th/nvim-cmp'},
+    { 'williamboman/mason.nvim', },
 
-{'L3MON4D3/LuaSnip'},
+    { 'williamboman/mason-lspconfig.nvim' },
+
+    { 'neovim/nvim-lspconfig' },
+
+    { 'hrsh7th/cmp-nvim-lsp' },
+
+    { 'L3MON4D3/LuaSnip' },
+
+    { "saadparwaiz1/cmp_luasnip" },
+
+    { "rafamadriz/friendly-snippets" },
+
+    { "hrsh7th/cmp-path" },
+
+    { "hrsh7th/cmp-cmdline" },
+
+    { "hrsh7th/cmp-buffer" },
+
+    { 'hrsh7th/nvim-cmp' },
 
 }
