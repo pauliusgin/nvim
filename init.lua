aac2c221d820dfe0e0cf84642688dc698f6f8684
Paulius <paulius.giniunas@gmail.com>
2024-06-11 19:24:11 +0300

    feat: add hardtime.nvim among other plugins

--- init.lua
+++ init.lua
@@ -8,13 +8,16 @@ vim.g.mapleader = " "
 
 local keymapOptions = { noremap = true, silent = true }
 
--- Ctrl-s to save and format the document with Prettier
-vim.keymap.set("n", "<C-s>", ":Prettier<CR> :w<CR>", keymapOptions)
+-- Ctrl-s to save
+vim.keymap.set("n", "<C-s>", ":w<CR>", keymapOptions)
 
--- Format the document with Prettier without saving
+-- Format the document with native linter
+vim.keymap.set("n", "<leader>=", "gg=G``", keymapOptions)
+
+-- Format the document with Prettier 
 vim.keymap.set("n", "<Leader>p", ":PrettierAsync<CR>", keymapOptions)
 
--- Ctrl-q to  quit
+-- Ctrl-q to quit
 vim.keymap.set("n", "<C-q>", ":q<CR>", keymapOptions)
 
 -- netrw
@@ -24,6 +27,10 @@ vim.keymap.set("n", "<C-q>", ":q<CR>", keymapOptions)
 -- neo-tree
 vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", keymapOptions)
 
+-- tabs
+vim.keymap.set({"n", "i", "v"}, "<C-m>", ":tabnext<CR>", keymapOptions)
+vim.keymap.set({"n", "i", "v"}, "<C-n>", ":tabprevious<CR>", keymapOptions)
+vim.keymap.set({"n", "i", "v"}, "<C-w>", ":tabclose<CR>", keymapOptions)
 
 -- split window
 vim.keymap.set("n", "<leader>sp", ":split<CR>", keymapOptions)
@@ -120,7 +127,7 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 -- mouse mode
-vim.opt.mouse = "a"
+-- vim.opt.mouse = "a"
 
 -- width
 vim.opt.colorcolumn = "80"
@@ -154,8 +161,9 @@ vim.api.nvim_set_hl(0, 'NonText', { bg = 'none', ctermbg = 'none' })
 
 -- {{{ PLUGINS
 
--- {{{ Lazy.nvim | A modern plugin manager for Neovim
--- 
+-- {{{ Lazy.nvim
+-- A modern plugin manager for Neovim
+-- https://github.com/folke/lazy.nvim
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -175,6 +183,8 @@ require("lazy").setup({
 
   -- {{{ nvim-treesitter 
   -- Treesitter configurations and abstraction layer for Neovim
+  -- https://github.com/nvim-treesitter/nvim-treesitter
+
   {
     "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
     config = function()
@@ -201,6 +211,8 @@ require("lazy").setup({
 
   -- {{{ Autoclose.nvim
   -- A minimalist Neovim plugin that auto pairs & closes brackets
+  -- https://github.com/m4xshen/autoclose.nvim
+
   {
     "m4xshen/autoclose.nvim",
     config = function()
@@ -209,8 +221,42 @@ require("lazy").setup({
   },
   -- }}}
 
+  -- {{{ nvim-surround
+  -- Surround selections, stylishly
+  -- https://github.com/kylechui/nvim-surround
+
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+
+    -- The three "core" operations can be done with the keymaps:
+
+    -- add: ys{motion}{char}
+    -- delete: ds{char} 
+    -- change: cs{target}{replacement}
+
+    -- For the following examples, * will denote the cursor position:
+
+    --           Old text                    Command         New text
+    -- --------------------------------------------------------------------------------
+    --     surr*ound_words             ysiw)           (surround_words)
+    --     *make strings               ys$"            "make strings"
+    --     [delete ar*ound me!]        ds]             delete around me!
+    --     remove <b>HTML t*ags</b>    dst             remove HTML tags
+    --     'change quot*es'            cs'"            "change quotes"
+    --     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+    --     delete(functi*on calls)     dsf             function calls
+  },
+  -- }}}
+
   -- {{{ Mason.nvim
   -- Install and manage LSP servers, DAP servers, linters, and formatters
+  -- https://github.com/williamboman/mason.nvim
+
   {
     "williamboman/mason.nvim",
     config = function()
@@ -221,6 +267,7 @@ require("lazy").setup({
 
   -- {{{ nvim-lspconfig
   -- A collection of LSP configs
+  -- https://github.com/neovim/nvim-lspconfig
 
   {
     "neovim/nvim-lspconfig",
@@ -244,6 +291,8 @@ require("lazy").setup({
 
   -- {{{ mason-lspconfig.nvim
   -- Bridges mason.nvim with the lspconfig plugin
+  -- https://github.com/williamboman/mason-lspconfig.nvim
+
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
@@ -261,8 +310,10 @@ require("lazy").setup({
   },
   -- }}}
 
-  -- {{{ telescope.nvim
+  -- {{{ telescope.nvim 
   -- A highly extendable fuzzy finder over lists
+  -- https://github.com/nvim-telescope/telescope.nvim
+
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
@@ -284,11 +335,12 @@ require("lazy").setup({
 
   -- {{{ vim-prettier
   -- A vim plugin wrapper for Prettier
+  -- https://github.com/prettier/vim-prettier
 
   {
     "prettier/vim-prettier",
     config = function()
-    	vim.g["prettier#config#config_precedence"] = "prefer-file"
+      vim.g["prettier#config#config_precedence"] = "prefer-file"
 
       vim.g["prettier#config#arrow_parens"] = "always"
       vim.g["prettier#config#bracket_spacing"] = "false"
@@ -302,71 +354,78 @@ require("lazy").setup({
       vim.g["prettier#config#semi"] = "true"
       vim.g["prettier#config#single_attribute_per_line"] = "false"
       vim.g["prettier#config#single_quote"] = "false"
-      vim.g["prettier#config#tab_width"] = "2"
+      vim.g["prettier#config#tab_width"] = "4"
       vim.g["prettier#config#trailing_comma"] = "es5"
       vim.g["prettier#config#use_tabs"] = "false"
     end 
   },
   -- }}}
-  
--- {{{ vim-cspell
--- Checks spelling by using cspell for Vim/Neovim
+
+  -- {{{ cspell.nvim
+  -- Support for cspell diagnostics and code actions.
+  -- https://github.com/davidmh/cspell.nvim
 
   {
-    "ryu-ichiroh/vim-cspell",
-
-    config = function()
-        vim.g.cspell_ext = {
-          ".txt", ".js", ".ts", ".jsx", ".tsx", ".html", ".css", "scss", ".md", ".json"
-        }
-        vim.g.cspell_config_file = vim.fn.expand('~/.cspell.json')
-
-    end
-  },
-  -- }}}
-
--- {{{ neo-tree.nvim
--- Plugin to browse the file system and other tree like structures
-
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    "davidmh/cspell.nvim",
     dependencies = {
+      "nvimtools/none-ls.nvim",
+      "nvim-lua/plenary.nvim"},
+      config = function()
+        local cspell = require('cspell')
+        require("null-ls").setup {
+          sources = {
+            cspell.diagnostics,
+            cspell.code_actions,
+          }
+        }
+      end
+    },
+    -- }}}
+
+    -- {{{ neo-tree.nvim
+    -- Plugin to browse the file system and other tree like structures
+    -- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
-    },
-    config = function()
+      },
+      config = function()
         require("neo-tree").setup({
-            window = {
-                width = 40,
-                mappings = {
-                    ["z"] = "close_all_nodes",
-                    ["Z"] = "expand_all_nodes",
-                    ["s"] = "open_vsplit",
-                    ["S"] = "open_split",
-                    ["."] = "set_root",
-                    ["H"] = "toggle_hidden",
-                },
+          window = {
+            width = 40,
+            mappings = {
+              ["z"] = "close_all_nodes",
+              ["Z"] = "expand_all_nodes",
+              ["s"] = "open_vsplit",
+              ["S"] = "open_split",
+              ["."] = "set_root",
+              ["H"] = "toggle_hidden",
             },
-            filesystem = {
-                filtered_items = {
-                    visible = false,
-                    hide_dotfiles = true,
-                    hide_gitignored = true,
-                    hide_hidden = true,
-                    hide_by_name = {
-                        "undo",
-                    },
-                },
+          },
+          filesystem = {
+            filtered_items = {
+              visible = false,
+              hide_dotfiles = true,
+              hide_gitignored = true,
+              hide_hidden = true,
+              hide_by_name = {
+                "undo",
+              },
             },
+          },
         })
-    end
-  },
-  -- }}}
+      end
+    },
+    -- }}}
 
-  -- {{{ lualine.nvim
+    -- {{{ lualine.nvim
     -- A blazing fast and easy to configure Neovim statusline written in Lua
+    -- https://github.com/nvim-lualine/lualine.nvim
 
     {
       'nvim-lualine/lualine.nvim',
@@ -413,9 +472,171 @@ require("lazy").setup({
           extensions = {}
         }
       end
+    },
+    -- }}}
+
+    -- {{{ gitsigns.nvim 
+    -- Super fast git decorations implemented purely in Lua.
+    -- https://github.com/lewis6991/gitsigns.nvim
+
+    {
+      "lewis6991/gitsigns.nvim",
+      config = function()
+        require('gitsigns').setup {
+          signs = {
+            add          = { text = '┃' },
+            change       = { text = '┃' },
+            delete       = { text = '_' },
+            topdelete    = { text = '‾' },
+            changedelete = { text = '~' },
+            untracked    = { text = '┆' },
+          },
+          signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+          numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+          linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+          word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+          watch_gitdir = {
+            follow_files = true
+          },
+          auto_attach = true,
+          attach_to_untracked = false,
+          current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+          current_line_blame_opts = {
+            virt_text = true,
+            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+            delay = 1000,
+            ignore_whitespace = false,
+            virt_text_priority = 100,
+          },
+          current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+          current_line_blame_formatter_opts = {
+            relative_time = false,
+          },
+          sign_priority = 6,
+          update_debounce = 100,
+          status_formatter = nil, -- Use default
+          max_file_length = 40000, -- Disable if file is longer than this (in lines)
+          preview_config = {
+            -- Options passed to nvim_open_win
+            border = 'single',
+            style = 'minimal',
+            relative = 'cursor',
+            row = 0,
+            col = 1
+          },
+          on_attach = function(bufnr)
+            local gitsigns = require('gitsigns')
+
+            local function map(mode, l, r, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map('n', ']c', function()
+              if vim.wo.diff then
+                vim.cmd.normal({']c', bang = true})
+              else
+                gitsigns.nav_hunk('next')
+              end
+            end)
+
+            map('n', '[c', function()
+              if vim.wo.diff then
+                vim.cmd.normal({'[c', bang = true})
+              else
+                gitsigns.nav_hunk('prev')
+              end
+            end)
+
+            -- Actions
+            map('n', '<leader>hs', gitsigns.stage_hunk)
+            map('n', '<leader>hr', gitsigns.reset_hunk)
+            map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+            map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+            map('n', '<leader>hS', gitsigns.stage_buffer)
+            map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+            map('n', '<leader>hR', gitsigns.reset_buffer)
+            map('n', '<leader>hp', gitsigns.preview_hunk)
+            map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+            map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+            map('n', '<leader>hd', gitsigns.diffthis)
+            map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+            map('n', '<leader>td', gitsigns.toggle_deleted)
+
+            -- Text object
+            map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+          end
+        }
+      end
+    },
+    -- }}}
+
+    -- {{{ Harpoon
+    -- Getting you where you want with the fewest keystrokes
+    -- https://github.com/ThePrimeagen/harpoon/tree/harpoon2
+
+    {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        local harpoon = require("harpoon")
+
+        harpoon:setup()
+
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+      end
+    },
+    --}}}
+
+    -- {{{ hardtime.nvim
+      -- Establish good command workflow and quit bad habit
+      -- https://github.com/m4xshen/hardtime.nvim
+
+      {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        opts = {}
+      },
+      -- }}}
+
+      -- {{{ Kanagawa.nvim | colorscheme
+      -- NeoVim dark colorscheme inspired by the colors of the famous painting by Katsushika Hokusai.
+      -- https://github.com/rebelot/kanagawa.nvim
+
+      {
+        "rebelot/kanagawa.nvim",
+        config = function()
+          require("kanagawa").setup({
+            transparent = true,
+            terminalColors = true,
+            colors = {
+              theme = {
+                all = {
+                  ui = {
+                    bg_gutter = "none",
+                  },
+                },
+              },
+            },
+          })
+          vim.cmd("colorscheme kanagawa-dragon")
+        end,
       }
-  -- }}}
+      -- }}}
 
-}, opts)
+    }, opts)
 
--- }}}
+    -- }}}
